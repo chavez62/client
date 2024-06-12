28f73e034d0d87342f96780193bdf7644646873f
chavez62 <louisatellite@gmail.com>
2024-06-12 16:59:44 -0700

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-primary text-white text-center p-4 mt-8 shadow-lg font-sans">
-      <p>&copy; 2024 My Blog. All rights reserved.</p>
+      <p>&copy; 2024 Dawg with a Blog. All rights reserved.</p>
       <div className="mt-2">
         <a href="#privacy" className="text-white hover:text-gray-400 mx-2">Privacy Policy</a>
         <a href="#terms" className="text-white hover:text-gray-400 mx-2">Terms of Service</a>
